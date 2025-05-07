a33e6680ea307c5f714f9fcec0ff19d4076260d0
Gareth Whitley <garethwhitleychard@gmail.com>
2025-05-07 11:59:22 +0100

    chore: update function name for jacquardSimilarity

--- js/lib/answers.js
+++ js/lib/answers.js
@@ -4,7 +4,7 @@ import { jacquardSimilarity } from "./helpers.js";
 export const markAnswers = () => {
   userInput.players.map(player => {
     player.answers.forEach((answer,index) => {
-      if (jaccardSimilarity(index, new Set(answer.answerValue.toLowerCase())) >= 1) {
+      if (jacquardSimilarity(index, new Set(answer.answerValue.toLowerCase())) >= 1) {
         player.answers[index].answerCorrect = true;
         player.score++;
       }
